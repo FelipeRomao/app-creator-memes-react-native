54c87705ef992bf7646c02759e95194174c2233c
Felipe Romao <feliperomao.a@gmail.com>
2019-03-26 21:40:56 -0300

    Recebendo dados do usuário

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 
 
 export default class PrimeiroApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text : ''};
+
+    this.mudarText = this.mudarText.bind(this);
+  }
+
+  mudarText(t) {
+    let string = this.state;
+   if(t.length > 0) {
+    string.text = 'Olá, ' + t; 
+   } else {
+    string.text = '';
+   }
+
+    this.setState(string);
+  }
+  
   render() {    
     return(
-      <View style={{flex : 1, flexDirection : 'row', justifyContent : 'center', alignItems : 'center'}}>
-        <View style={{height:50, width:50, backgroundColor: 'red'}}></View>
-        <View style={{height:50, width:50, backgroundColor: 'blue'}}></View>
-        <View style={{height:50, width:50, backgroundColor: 'green'}}></View>
+      <View style={styles.container}>
+        <Text style={{paddingLeft : 10}}>Nome</Text>
+        <TextInput style={styles.input} placeholder='Digite seu nome' onChangeText={this.mudarText} />
+        
+        <Text style={styles.text}>{this.state.text}</Text>
       </View>
-      
-
-      //flexDirection : 'row' / flexDirection : 'column'
-      //justifyContent : flex-start / flex-end / center / space-between / space-around
-      //alignItems : flex-start / flex-end / center / stretch
     );
   }
+  
 }
+
+const styles = StyleSheet.create({
+  container : {
+    paddingTop : 30
+  },
+
+  input : {
+    height : 40,
+    borderWidth : 1,
+    borderColor : 'blue',
+    margin : 10,
+    padding : 10,
+  },
+  
+  text : {
+    fontSize : 20,
+    textAlign : 'center', 
+  },
+});
